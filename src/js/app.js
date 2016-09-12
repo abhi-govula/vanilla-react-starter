73117b8bd4406b0b8597235bde8b3033bb928122
Abhi Govula <abhi.govula@scriptbees.com>
2016-09-12 20:56:24 +0530

    Initializes a basic react project

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+// ReactDOM.render(
+// <Provider store={createStoreWithMiddleware(reducers)}>
+//     <Router history={hashHistory}>
+//         <Route path="/" component={Layout}>
+//             <IndexRoute component={Featured}></IndexRoute>
+//             <Route path="about" component={About}></Route>
+//             <Route path="employees" component={Employees}></Route>
+//             <Route name="employee" path="employee/:id" component={EmployeeProfile}></Route>
+//             <Route path="contact" component={Contact}></Route>
+//         </Route>
+//     </Router>
+//  </Provider>
+//     , document.getElementById('app'));
+ReactDOM.render(<p>hi</p>, document.getElementById('app'));
